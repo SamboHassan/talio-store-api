d3fa1a12904e2d48ff8a80069d261a573e0d3fda
Hassan Sambo <sambohasan@gmail.com>
2024-01-24 16:21:20 +0100

    feat: add store for endpoints

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask
+from flask_smorest import abort
 from flask import request
+from db import stores, items
+import uuid
 
 app = Flask(__name__)
 
-stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
 
-
-@app.get("/stores")  # http://127.0.0.1:5000/stores
-def get_stores():
-    return {"stores": stores}
-
-
-@app.post("/stores")  # http://127.0.0.1:5000/stores
+@app.post("/store")  # http://127.0.0.1:5000/store
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+
+@app.get("/store")  # http://127.0.0.1:5000/store
+def get_stores():
+    return {"stores": list(stores.values())}
+
+
+@app.get("/store/<string:store_id>")  # http://127.0.0.1:5000/store/storeName
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
